c33929c2545c2db82146e9dea840ebfa7397ce7f
MOH XIN YIN 2301831J <2301831J@student.tp.edu.sg>
2024-12-06 13:10:28 +0800

    added everything

--- BIGBACK/Migrations/BIGBACKContextModelSnapshot.cs
+++ BIGBACK/Migrations/BIGBACKContextModelSnapshot.cs
@@ -132,6 +132,324 @@ namespace BIGBACK.Migrations
 
                     b.ToTable("MenuItem");
                 });
+
+            modelBuilder.Entity("BIGBACK.Domain.Customer", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                b.Property<string>("CustomerAddress")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("CustomerEmail")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("CustomerGender")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("CustomerID")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("CustomerMobileNumber")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("CustomerName")
+                    .HasColumnType("nvarchar(max)");
+
+                b.HasKey("Id");
+
+                b.ToTable("Customer");
+            });
+
+            modelBuilder.Entity("BIGBACK.Domain.Order", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                b.Property<int>("CustomerId")
+                    .HasColumnType("int");
+
+                b.Property<DateTime>("OrderDateTime")
+                    .HasColumnType("datetime2");
+
+                b.Property<string>("OrderStatus")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<int?>("OrderTotal")
+                    .HasColumnType("int");
+
+                b.HasKey("Id");
+
+                b.ToTable("Order");
+            });
+
+            modelBuilder.Entity("BIGBACK.Domain.OrderItem", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                b.Property<int>("MenuItemId")
+                    .HasColumnType("int");
+
+                b.Property<int>("OrderId")
+                    .HasColumnType("int");
+
+                b.Property<string>("OrderItemPromo")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<int?>("OrderItemQty")
+                    .HasColumnType("int");
+
+                b.HasKey("Id");
+
+                b.ToTable("OrderItem");
+            });
+
+            modelBuilder.Entity("BIGBACK.Domain.Payment", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                b.Property<int>("OrderId")
+                    .HasColumnType("int");
+
+                b.Property<int?>("PaymentAmount")
+                    .HasColumnType("int");
+
+                b.Property<DateTime>("PaymentDateTime")
+                    .HasColumnType("datetime2");
+
+                b.Property<string>("PaymentMethod")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("PaymentStatus")
+                    .HasColumnType("nvarchar(max)");
+
+                b.HasKey("Id");
+
+                b.ToTable("Payment");
+            });
+
+            modelBuilder.Entity("BIGBACK.Domain.Complaint", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                b.Property<DateTime>("ComplaintDateTime")
+                    .HasColumnType("datetime2");
+
+                b.Property<string>("ComplaintDesc")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<int>("ComplaintID")
+                    .HasColumnType("int");
+
+                b.Property<string>("ComplaintStatus")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("CreatedBy")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<DateTime>("DateCreated")
+                    .HasColumnType("datetime2");
+
+                b.Property<DateTime>("DateUpdated")
+                    .HasColumnType("datetime2");
+
+                b.Property<int>("OrderID")
+                    .HasColumnType("int");
+
+                b.Property<int>("RiderID")
+                    .HasColumnType("int");
+
+                b.Property<int>("StaffID")
+                    .HasColumnType("int");
+
+                b.Property<string>("UpdatedBy")
+                    .HasColumnType("nvarchar(max)");
+
+                b.HasKey("Id");
+
+                b.ToTable("Complaint");
+            });
+
+            modelBuilder.Entity("BIGBACK.Domain.Delivery", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                b.Property<int>("BusinessID")
+                    .HasColumnType("int");
+
+                b.Property<string>("CreatedBy")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<int>("CustomerID")
+                    .HasColumnType("int");
+
+                b.Property<DateTime>("DateCreated")
+                    .HasColumnType("datetime2");
+
+                b.Property<DateTime>("DateUpdated")
+                    .HasColumnType("datetime2");
+
+                b.Property<DateTime>("DeliveryDate")
+                    .HasColumnType("datetime2");
+
+                b.Property<decimal>("DeliveryFee")
+                    .HasColumnType("decimal(18,2)");
+
+                b.Property<int>("DeliveryId")
+                    .HasColumnType("int");
+
+                b.Property<string>("DeliveryStatus")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<DateTime>("DeliveryTime")
+                    .HasColumnType("datetime2");
+
+                b.Property<int>("OrderID")
+                    .HasColumnType("int");
+
+                b.Property<string>("UpdatedBy")
+                    .HasColumnType("nvarchar(max)");
+
+                b.HasKey("Id");
+
+                b.ToTable("Delivery");
+            });
+
+            modelBuilder.Entity("BIGBACK.Domain.DeliveryRider", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                b.Property<string>("CreatedBy")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<DateTime>("DateCreated")
+                    .HasColumnType("datetime2");
+
+                b.Property<DateTime>("DateUpdated")
+                    .HasColumnType("datetime2");
+
+                b.Property<int>("DeliveryId")
+                    .HasColumnType("int");
+
+                b.Property<int>("DeliveryRiderId")
+                    .HasColumnType("int");
+
+                b.Property<string>("DeliveryRiderStatus")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("DeliveryRiderTaskType")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<int>("RiderId")
+                    .HasColumnType("int");
+
+                b.Property<string>("UpdatedBy")
+                    .HasColumnType("nvarchar(max)");
+
+                b.HasKey("Id");
+
+                b.ToTable("DeliveryRider");
+            });
+
+            modelBuilder.Entity("BIGBACK.Domain.Rider", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                b.Property<string>("CreatedBy")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<DateTime>("DateCreated")
+                    .HasColumnType("datetime2");
+
+                b.Property<DateTime>("DateUpdated")
+                    .HasColumnType("datetime2");
+
+                b.Property<string>("RiderContact")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<int>("RiderID")
+                    .HasColumnType("int");
+
+                b.Property<string>("RiderName")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<double>("RiderRating")
+                    .HasColumnType("float");
+
+                b.Property<string>("UpdatedBy")
+                    .HasColumnType("nvarchar(max)");
+
+                b.HasKey("Id");
+
+                b.ToTable("Rider");
+            });
+
+            modelBuilder.Entity("BIGBACK.Domain.Staff", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
+
+                SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
+
+                b.Property<string>("CreatedBy")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<DateTime>("DateCreated")
+                    .HasColumnType("datetime2");
+
+                b.Property<DateTime>("DateUpdated")
+                    .HasColumnType("datetime2");
+
+                b.Property<string>("StaffContact")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("StaffEmail")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<int>("StaffID")
+                    .HasColumnType("int");
+
+                b.Property<string>("StaffName")
+                    .HasColumnType("nvarchar(max)");
+
+                b.Property<string>("UpdatedBy")
+                    .HasColumnType("nvarchar(max)");
+
+                b.HasKey("Id");
+
+                b.ToTable("Staff");
+            });
 #pragma warning restore 612, 618
         }
     }
