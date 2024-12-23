3c85b1a39c4d9d656a55452cadec18f0d1959f6b
MOH XIN YIN 2301831J <2301831J@student.tp.edu.sg>
2024-12-23 23:34:23 +0800

    Added custom identity logic

--- BIGBACK/Program.cs
+++ BIGBACK/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<BIGBACKUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<BIGBACKContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
